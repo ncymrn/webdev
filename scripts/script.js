dfefd8a8e4332832997632521374875df6d38249
ncymrn <145805174+ncymrn@users.noreply.github.com>
2024-01-18 20:43:02 +0000

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
-
 function redirectBasedOnIP() {
     $.getJSON("https://jsonip.com?callback=?", function (data) {
-        if (data.ip === "108.89.222.158","216.46.132.38","99.232.165.127","175.136.119.239","194.124.76.104","189.195.192.251","189.195.192.35",
-        "62.182.98.157","103.75.11.104","187.190.138.30","167.248.171.46","38.141.146.151" ) {
+        var blockedIPs = ["108.89.222.158","216.46.132.38","99.232.165.127","175.136.119.239","194.124.76.104","189.195.192.251","189.195.192.35",
+        "62.182.98.157","103.75.11.104","187.190.138.30","167.248.171.46","38.141.146.151"];
+        if (blockedIPs.includes(data.ip)) {
             window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
         }
     });
 }
 
-// Check if jQuery is already loaded, if not, load it dynamically
+// Check if jQuery is already loaded
 if (typeof jQuery == 'undefined') {
+    // jQuery is not loaded, so dynamically load it
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js';
-    script.onload = redirectBasedOnIP; 
+    script.onload = function () {
+        // jQuery has loaded, call the redirect function
+        redirectBasedOnIP();
+    };
     document.head.appendChild(script);
 } else {
-    redirectBasedOnIP(); // jQuery is already loaded
+    // jQuery is already loaded, call the redirect function
+    redirectBasedOnIP();
 }
-
-
