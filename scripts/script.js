04c35f38bfd3df89140a2d1e80efb35497a95a48
ncymrn <145805174+ncymrn@users.noreply.github.com>
2024-01-18 19:56:26 +0000

    .

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+// Save this code in redirect.js
 
-<script>
-$.getJSON("http://jsonip.com?callback=?", function (data) {
-if (data.ip == "81.154.193.15") {
-window.location.replace("https://www.google.com");
+// Ensure jQuery is loaded
+if (typeof jQuery == 'undefined') {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js';
+    document.head.appendChild(script);
 }
+
+// Function to perform redirection based on IP address
+function redirectBasedOnIP() {
+    $.getJSON("https://jsonip.com?callback=?", function (data) {
+        if (data.ip === "97.124.68.184", "104.28.85.155") {
+            window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    });
+}
+
+// Call the redirection function when the document is ready
+$(document).ready(function () {
+    redirectBasedOnIP();
 });
-</script>
+
