c35bc6884700f13f092c6d414bae5584450451f7
ncymrn <145805174+ncymrn@users.noreply.github.com>
2024-01-18 20:10:19 +0000

    .

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@
 // Function to perform redirection based on IP address
 function redirectBasedOnIP() {
     $.getJSON("https://jsonip.com?callback=?", function (data) {
-        if (data.ip === "97.124.68.184", "81.154.193.151") {
+        if (data.ip === "38.141.146.151", "38.141.146.15", "216.46.132.38", "99.232.165.127", "175.136.119.239", "108.89.222.158",
+        "189.195.192.251", "189.195.192.35", "62.182.98.157", "187.190.138.30", "103.75.11.104", "167.248.171.46", "194.124.76.104") {
             window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
         }
     });
